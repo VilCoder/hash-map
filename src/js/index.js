7c6bc3c61ede878d445616ab67548106db8cad55
VilCoder <176792549+VilCoder@users.noreply.github.com>
2025-05-15 12:25:54 -0500

    Feat: Add hashSet structure
    - Added hashSet class with  methods for manipulating its elements such
    as hash, at, set, get, resize, and more...

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,22 @@
-import HashMap from "./hashMap.js";
+import HashSet from "./hashSet.js";
 
-const test = new HashMap();
+const test = new HashSet();
 
-test.set("apple", "red");
-test.set("banana", "yellow");
-test.set("carrot", "orange");
-test.set("dog", "brown");
-test.set("elephant", "gray");
-test.set("frog", "green");
-test.set("grape", "purple");
-test.set("hat", "black");
-test.set("ice cream", "white");
-test.set("jacket", "blue");
-test.set("kite", "pink");
-test.set("lion", "golden");
+test.set("apple");
+test.set("banana");
+test.set("carrot");
+test.set("dog");
+test.set("elephant");
+test.set("frog");
+test.set("grape");
+test.set("hat");
+test.set("ice cream");
+test.set("jacket");
+test.set("kite");
+test.set("lion");
 
-console.log(test.get("hat"));
-console.log(test.length());
-test.clear();
-console.log(test.length());
-console.log(test.has("grape"));
+console.log(test.length())
+console.log(test.keys())
+console.log(test.remove("apple"))
+console.log(test.keys())
+console.log(test.length())
